993cd54aae4d0003c9b209d0724eed0f3d4eb501
Nickrocky213 <nickgk505@gmail.com>
2018-05-13 14:08:12 -0400

    Addition of Creative Tabs

--- src/main/java/com/azarius/AzarianConquest.java
+++ src/main/java/com/azarius/AzarianConquest.java
@@ -1,8 +1,11 @@
 package com.azarius;
 
+import com.azarius.init.ItemInit;
 import com.azarius.proxy.CommonProxy;
 import com.azarius.utils.ACEventHandler;
 import com.azarius.utils.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
@@ -16,6 +19,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Reference.MODID, version = Reference.VERSION, name = Reference.NAME)
 public class AzarianConquest
 {
+	public static final CreativeTabs tabACFood = new CreativeTabs("tabACFood") { 
+		@Override public ItemStack getTabIconItem() {
+			return new ItemStack(ItemInit.glazedDonut);
+		}	
+	};
+	
 	public static ACEventHandler handler = new ACEventHandler();
 	
     @Instance
