941df72cb72872eb91bf8464288134bfb48a2e29
Nickrocky213 <nickgk505@gmail.com>
2018-05-13 16:27:52 -0400

    Food

--- src/main/java/com/azarius/init/ItemInit.java
+++ src/main/java/com/azarius/init/ItemInit.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.azarius.AzarianConquest;
 import com.azarius.api.bases.FoodBase;
 import com.azarius.api.bases.ItemBase;
+import com.azarius.utils.enums.items.FoodList;
 import com.azarius.utils.enums.items.GeneralItemList;
 
 import net.minecraft.item.Item;
@@ -17,6 +18,7 @@ public class ItemInit {
 public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final ItemFood glazedDonut = new FoodBase(GeneralItemList.GLAZEDDONUT.getUnlocalizedName(), GeneralItemList.GLAZEDDONUT.getRegistryName(), 2, 0.3F, false, AzarianConquest.tabACFood);
+	public static final Item amaranth = new ItemBase(FoodList.AMARANTH.getUnlocalizedName(), FoodList.AMARANTH.getRegistryName());
 	
 	@SubscribeEvent
 	public void onItemRegister(Register<Item> event) {
---
+++ src/main/java/com/azarius/utils/enums/items/FoodList.java
@@ -0,0 +1,22 @@
+package com.azarius.utils.enums.items;
+
+public enum FoodList {
+	
+	AMARANTH("amaranth", "ItemAmaranth");
+	
+	private String unloc;
+	private String registry;
+	
+	FoodList(String unloc, String registry){
+		this.unloc = unloc;
+		this.registry = registry;
+	}
+	
+	public String getUnlocalizedName() {
+		return unloc;
+	}
+	
+	public String getRegistryName() {
+		return registry;
+	}
+}
