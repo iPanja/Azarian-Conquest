d392199607fe67c24a2244a1fcfc45c6c29958a1
iPanja <fhenneman@comcast.net>
2018-05-14 20:29:50 -0400

    Removed World Data (Attempts)

--- src/main/java/com/azarius/utils/ACEventHandler.java
+++ src/main/java/com/azarius/utils/ACEventHandler.java
@@ -1,28 +1,25 @@
 package com.azarius.utils;
 
-import com.azarius.utils.worlddata.WorldData;
-
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import scala.Console;
 
 public class ACEventHandler {
 	@SubscribeEvent
-	public void  playerInteract(PlayerInteractEvent e) {
-		
-	}
-	
-	WorldData data;
-	
-	@SubscribeEvent
     public void RightClickBlock(PlayerInteractEvent.RightClickItem event) {
-    	//Load World Configuration
-		data = data.getInstance(Minecraft.getMinecraft().world);
-		NBTTagCompound nbt = new NBTTagCompound();
-		nbt.setInteger("Yeet", nbt.getInteger("Yeet")+1);
-		data.writeToNBT(nbt);
-		Console.out().println("Yeet is now at: " + nbt.getInteger("Yeet"));
+		if(event.getEntity() == Minecraft.getMinecraft().player) {
+			
+		}
     }
+	@SubscribeEvent
+	public void EntityJoinWorldEvent(EntityJoinWorldEvent event) {
+		if(event.getEntity() == Minecraft.getMinecraft().player) {
+			
+		}
+	}
 }
